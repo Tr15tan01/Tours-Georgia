697f324a016ab0d40d969f7027064324d2c36010
tr15tan01 <t.varamashvili@gmail.com>
2022-09-15 21:49:27 +0400

    changed maca to mari

--- src/components/navigation.jsx
+++ src/components/navigation.jsx
@@ -16,7 +16,7 @@ export const Navigation = (props) => {
             <span className='icon-bar'></span>{' '}
           </button>
           <a className='navbar-brand page-scroll' href='#page-top'>
-            MACA Tours Georgia
+            MARI Tours Georgia
           </a>{' '}
         </div>
 
